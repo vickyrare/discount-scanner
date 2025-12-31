77b6e8ba8d3389c73a94f8078fa39b831fa67630
Waqqas Sharif <waqqas.sharif@oracle.com>
2025-12-31 12:35:29 +1100

    Add custom app icons and enable release signing
    Added new launcher icons for Android and iOS, updated AndroidManifest and iOS asset catalog to use the new icons, and configured Android release signing with key.properties. Updated dependencies to include flutter_launcher_icons, adjusted project version to 1.0.1+2, and enabled asset inclusion for images. Also updated build and settings files for proper icon and signing integration.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,11 +1,23 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+fun localProperties(): Properties {
+    val properties = Properties()
+    val localPropertiesFile = rootProject.file("local.properties")
+    if (localPropertiesFile.exists()) {
+        properties.load(FileInputStream(localPropertiesFile))
+    }
+    return properties
+}
+
 android {
+    // ... (rest of your android configuration)
     namespace = "com.cistemcode.discount_scanner"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
@@ -19,11 +31,24 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    signingConfigs {
+        create("release") {
+            val keyProperties = Properties()
+            val keyPropertiesFile = rootProject.file("key.properties")
+            if (keyPropertiesFile.exists()) {
+                keyProperties.load(FileInputStream(keyPropertiesFile))
+                keyAlias = keyProperties.getProperty("keyAlias")
+                keyPassword = keyProperties.getProperty("keyPassword")
+                storeFile = file(keyProperties.getProperty("storeFile"))
+                storePassword = keyProperties.getProperty("storePassword")
+            } else {
+                throw GradleException("Could not find key.properties file. Please create one in the android directory with your signing key details.")
+            }
+        }
+    }
+
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.cistemcode.discount_scanner"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,9 +57,7 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
